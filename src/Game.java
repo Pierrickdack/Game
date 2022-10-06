9c94f0514b0b64665fcea8b639e7c2f66c4b169e
Pierrick <kamelapierrick@gmail.com>
2022-10-06 14:58:43 +0100

    Petit jeu qui consiste à retrouver le prix exacte.

---
+++ src/Game.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    
+    public static void main (String[] args){
+
+        Random r = new Random();
+        int essai = 0;
+        int justprix = r.nextInt(200);
+        int prix = 0;
+
+        while(justprix != prix){
+
+            System.out.println("Entrez un numéro entre 0 et 200");
+            Scanner scan = new Scanner(System.in);
+
+
+            if (justprix > prix){
+                System.out.println("C'est plus !");
+            } else {
+                System.out.println("C'est moins !");
+            }
+
+            prix = scan.nextInt();
+            essai++;
+
+        }
+
+        System.out.println("Bravo ! le juste prix est : " + justprix + "\n");
+        System.out.println("En " + essai + " essais.");
+
+    }
+    
+}
